42827d3c755faa019c03d29e1d516b4c2eb155ea
jonessilva <jones7silva@gmail.com>
2018-10-25 19:33:19 -0300

    criado novo projeto em 25-10-2018

---
+++ inicio/for.py
@@ -0,0 +1,9 @@
+#declaraçao de variaveis
+soma=0
+#estrutura de repetiçao "PARA"
+for cont in range (1,10):
+    num=int(input('Digite um numer'))
+    soma=soma+num
+print(' Os valores sao :',soma)
+
+   
